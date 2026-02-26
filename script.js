b95879f5fec3013bade6dfff531637667d510c47
bingambini <136372031+bingambini@users.noreply.github.com>
2026-02-27 02:06:18 +0400

    script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ tg.setBackgroundColor('#ffffff');
 
 // --- კონფიგურაცია და მონაცემთა საცავი ---
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbwBUxp6_zCxMDZktZ67GGEqJO9siVIJL5f9lOY4aErU-jJrb08i0wYp5Fu5Dv9rEfm_xw/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbx9yLsI7CRDCKeNCVhjDWRS_ZNSRipr97XdLMxpDmS_K6sfES2E9OqaNfuf7UkQy32XJg/exec'
 };
 
 // --- აპლიკაციის მდგომარეობა ---
